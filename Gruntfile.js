0bf4f9e1a5fc00fdb1c4a2043224d58261043d1d
Martí Adrogué <marti.adrogue@gmail.com>
2016-09-03 18:57:29 +0200

    Rewrite js tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,6 +27,53 @@ module.exports = function (grunt) {
         },
       },
     },
+    concat: {
+      js: {
+        src: ['assets/js/block/**/*.js'],
+        dest: 'assets/js/dist/build.js',
+      },
+      resource: {
+        files: [
+          {
+            expand: true,
+            cwd: 'assets/js/raw',
+            src: '**/*.json',
+            dest: 'js/raw',
+          },
+        ],
+      },
+    },
+    uglify: {
+      options: {
+        compress: {
+          drop_console: false,
+        },
+        global_defs: {
+          DEBUG: false,
+        },
+        dead_code: true,
+        preserveComments: false,
+        banner: "'use strict';",
+        process: function (src, filepath) {
+          return src.replace(/(^|\n)[ \t]*('use strict'|"use strict");?\s*/g, '$1');
+        },
+      },
+      build: {
+        files: {
+          'js/main.js': ['assets/js/build.js'],
+        },
+      },
+      lib: {
+        files: [
+          {
+            expand: true,
+            cwd: 'assets/js/lib',
+            src: '**/*.*',
+            dest: 'js',
+          },
+        ],
+      },
+    },
     imagemin: {
       dynamic: {
         options: {
@@ -55,8 +102,10 @@ module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['less', 'cssmin', 'imagemin']);
+  grunt.registerTask('default', ['less', 'cssmin', 'concat', 'uglify', 'imagemin']);
 };
