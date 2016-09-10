b0d48d486c9640ced329621d22a61190576f4c66
Martí Adrogué <marti.adrogue@gmail.com>
2016-09-10 19:35:59 +0200

    Load sass plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -114,6 +114,7 @@ module.exports = function (grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
